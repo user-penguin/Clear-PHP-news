4def46bb49a6dd677444307b4067d413a7cda01f
user-penguin <kobzev96@gmail.com>
2021-02-11 23:42:43 +0700

    add templates dir

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+
+$autoloaderName = 'autoloader.php';
+$basePath = dirname(__FILE__);
+$templatesPath  = $basePath . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
+
+require_once $templatesPath . "base.php";
---
+++ templates/base.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <body>
+
+    <?php
+    ?>
+
+        <h1>My First Heading</h1>
+        <p>My first paragraph.</p>
+    </body>
+</html>
