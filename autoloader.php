a6dad7fd0a52a0d997b4fb30c33f8d2e6d941f80
user-penguin <kobzev96@gmail.com>
2021-02-24 23:01:35 +0700

    fill autoloader, edit index

--- autoloader.php
+++ autoloader.php
@@ -1,1 +1,19 @@
 <?php
+
+define('BASE_DIR', __DIR__);
+
+spl_autoload_register(function ($class) {
+    $pathParts = explode('\\', $class);
+    $last = array_pop($pathParts);
+    $last .= '.php';
+    $required = '';
+    foreach ($pathParts as $part) {
+        $required = '/' . array_pop($pathParts) . $required;
+    }
+    $required = $required . '/' . $last;
+    $required = BASE_DIR . str_replace('\\', '/', $required);
+
+    if (file_exists($required)) {
+        require_once $required;
+    }
+});
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
+define('AUTOLOADER_NAME', 'autoloader.php');
+require __DIR__ . DIRECTORY_SEPARATOR . AUTOLOADER_NAME;
 
-$autoloaderName = 'autoloader.php';
+use core\Router;
 $basePath = dirname(__FILE__);
 $templatesPath  = $basePath . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
+
+if (Router::getRoute($_SERVER['REQUEST_URI'])) {
+    echo 'lol';
+}
 
 require_once $templatesPath . "base.php";
